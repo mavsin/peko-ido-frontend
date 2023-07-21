7d301a630a1bf597b79ba3bea2c62917779bdb99
mr-new-0509 <cliffahleongtech@gmail.com>
2023-07-21 18:37:48 +0900

    DP - frontend

---
+++ app/components/Input.tsx
@@ -0,0 +1,34 @@
+import type { ReactNode } from 'react';
+
+/* --------------------------------------------------------------------------- */
+
+interface IProps {
+  className?: string;
+  children?: ReactNode | string;
+  classNameOfInput?: string;
+  startAdornment?: ReactNode;
+  endAdornment?: ReactNode;
+  error?: boolean;
+  [key: string]: any;
+}
+
+/* --------------------------------------------------------------------------- */
+
+export default function Input({ className = '', classNameOfInput = '', startAdornment, endAdornment, error, ...others }: IProps) {
+  return (
+    <div
+      className={`flex items-center gap-2 px-3 py-2 rounded bg-transparent border border-transparent ${className} ${error ? '!border-red-500' : ''}`}
+    >
+      {startAdornment ? (
+        <div>{startAdornment}</div>
+      ) : (<></>)}
+      <input
+        className={`flex-1 focus:outline-none w-full bg-transparent text-gray-100 placeholder:text-gray-700 ${classNameOfInput}`}
+        {...others}
+      />
+      {endAdornment ? (
+        <div>{endAdornment}</div>
+      ) : (<></>)}
+    </div>
+  );
+}
