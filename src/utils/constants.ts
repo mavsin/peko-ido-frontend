c355b2cc2a1a929213d52dffb30bc532fa5ce273
mr-new-0509 <cliffahleongtech@gmail.com>
2023-08-01 11:10:30 +0900

    compare vercel and netlify

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const SOCIAL_LINKS: Array<ISocialLink> = [
   //   id: 1,
   //   icon: "ic:baseline-discord",
   //   href: "https://discord.gg/MXR36KK8"
-  // },
+  // }, 
   {
     id: 2,
     icon: "mdi:twitter",
