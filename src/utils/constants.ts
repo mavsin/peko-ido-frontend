fb683442dd004295dae42d2905e2e8c1f3fb5d15
mr-new-0509 <cliffahleongtech@gmail.com>
2023-08-01 10:49:44 +0900

    discord removed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,11 @@
 import type { ISaleInfo, ISocialLink } from "./interfaces";
 
 export const SOCIAL_LINKS: Array<ISocialLink> = [
-  {
-    id: 1,
-    icon: "ic:baseline-discord",
-    href: "https://discord.gg/MXR36KK8"
-  },
+  // {
+  //   id: 1,
+  //   icon: "ic:baseline-discord",
+  //   href: "https://discord.gg/MXR36KK8"
+  // },
   {
     id: 2,
     icon: "mdi:twitter",
